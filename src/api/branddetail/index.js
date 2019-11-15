50be7ea7cd4c8fd3b5c58ca20bfea7a0e83afbf8
BK1917wangshuang <2667415248@qq.com>
2019-11-15 14:40:43 +0800

    项目1

--- src/api/branddetail/index.js
+++ src/api/branddetail/index.js
@@ -13,7 +13,6 @@ export const branddetailsApi = (brand_id) => http({
     method: "get",
     url: "/index.php?r=brand/branddetaillist",
     data: {
-     
         token: "7dd11b2265ef658c16f8b674630d0354",
         brand_id:brand_id,
         page: 1,
